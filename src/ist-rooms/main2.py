c0016f884aef6165a3507160eb4de9f01dc74108
Robert <popescu.robert.octavian@gmail.com>
2017-01-28 07:13:26 +0200

    ndb-adding-rooms

--- src/ist-rooms/main2.py
+++ src/ist-rooms/main2.py
@@ -14,19 +14,24 @@ def index():
 def do_login():
     # get ID number from the page and see if it is in database
     id_no = request.forms.get('id_number')
-    # OBS: what comes from the form is string
-    # todo: make query to the database and see if ID number is in
+    print id_no
     admin_id = '0'
     db_id = '11'
     db_username = 'Robert'
+    # OBS: what comes from the form is string
     if id_no == db_id:
-    	# route to checkin in rooms available
-    	return '<p>Welcome %s.</p>' % db_username
+        # route to checkin in rooms available
+        return '<p> Welcome %s. </p>' % db_username
     elif id_no == admin_id:
-    	# route to give priviledges to add rooms to the database
-    	pass
+        # route to give priviledges to add rooms to the database
+        # todo: make query to the database and see if ID number is in 
+        return '''<p> Welcome admin. </p>
+        <p> <form action="/campus" method = "get">
+                <input type="submit" value="Campuses">
+            </form> 
+        </p>''' 
     else:
-    	return '<p>The ID %s is not registered, please register.</p>' % id_no
+        return '<p>The ID %s is not registered, please register.</p>' % id_no
 
 @app.route('/register', method='POST')
 def do_register():
@@ -54,6 +59,27 @@ def list_campus():
     	# print campus_names
     # this should return to JavaScript it knows how to handle lists
     return "<p>Campuses: %s</p>" % campus_names
+
+class Room(ndb.Model):
+    # model an individual room with name, capacity, date
+    name = ndb.StringProperty()
+    capacity = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+@app.route('/add/<room>')
+def add_room(room):
+    print 'add room'
+    r = Room(name = room, capacity = 30)
+    key = r.put()
+    return 'Room %s added with key %s' % (r, str(key.id()))
+
+@app.route('/showrooms')
+def show_rooms():
+    ret = ''
+    rooms = Room.query()
+    for r in rooms:
+        ret += 'ID: ' + str(r.key.id()) + ' Name: ' + r.name + ' Capacity: ' + str(r.capacity) + ' Date: ' + str(r.date) + '<br>'
+    return ret
 
 # Define a handler for 404 errors.
 @app.error(404)
