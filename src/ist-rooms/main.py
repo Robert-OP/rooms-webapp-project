d1382169d178455489fa8bb7bef985e3e09a3787
Robert <popescu.robert.octavian@gmail.com>
2017-01-29 22:34:48 +0200

    version 2.1

--- src/ist-rooms/main.py
+++ src/ist-rooms/main.py
@@ -15,14 +15,13 @@ def index():
 def do_login():
     # get ID number from the page and see if it is in database
     id_no = request.forms.get('id_number')
-    print id_no
     admin_id = '0'
     db_id = '11'
     db_username = 'Robert'
     # OBS: what comes from the form is string
     if id_no == db_id:
         # route to checkin in rooms available
-        return '<p> Welcome %s. </p>' % db_username
+        return template('student.html')
     elif id_no == admin_id:
         # route to give priviledges to add rooms to the database
         # todo: make query to the database and see if ID number is in 
@@ -32,13 +31,14 @@ def do_login():
 
 class Student(ndb.Model):
     name = ndb.StringProperty()
+    checkin = ndb.IntegerProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 @app.route('/register', method='POST')
 def do_register():
     # get username from the page and assign an ID and insert in database
     username = request.forms.get('username')
-    student = Student(name = username)
+    student = Student(name=username, checkin=0)
     key = student.put()
     # todo: make query to the database and see what is the last ID number
     return 'Hello %s, this is your ID: %s' % (student, str(key.id()))
@@ -54,13 +54,13 @@ def list_campus(id):
 class Room(ndb.Model):
     # model an individual room with name, capacity, date
     name = ndb.StringProperty()
-    occupancy = 0 
+    occupancy = ndb.IntegerProperty()
     capacity = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
 
 @app.post('/add/<room>/<capacity>')
 def add_room(room, capacity):
-    r = Room(name = room, capacity = capacity)
+    r = Room(name=room, occupancy=0, capacity=capacity)
     key = r.put()
     return 'Room %s added with key %s' % (r, str(key.id()))
 
@@ -69,7 +69,7 @@ def show_rooms():
     out = ''
     rooms = Room.query()
     for r in rooms:
-        out += 'ID: ' + str(r.key.id()) + ' Name: ' + r.name + ' Capacity: ' + str(r.capacity) + ' Date: ' + str(r.date) + '<br>'
+        out += ' Name: ' + r.name + ' |Occupancy: ' + str(r.occupancy) + ' |Total Capacity: ' + r.capacity + ' |ID: ' + str(r.key.id()) + ' |Date: ' + str(r.date) + '<br>'
     return out
 
 @app.route('/students')
@@ -77,7 +77,7 @@ def show_rooms():
     out = ''
     students = Student.query()
     for s in students:
-        out += 'ID: ' + str(s.key.id()) + ' Name: ' + s.name + ' Date: ' + str(s.date) + '<br>'
+        out += 'ID: ' + str(s.key.id()) + ' |Name: ' + s.name + ' | Enrol Date: ' + str(s.date) + '<br>'
     return out
 
 @app.get('/spaces/<id>')
@@ -94,8 +94,6 @@ def req1():
     response.content_type = 'application/json'
     return response.read()
 
-
-# Define a handler for 404 errors.
 @app.error(404)
 def error_404(error):
     """Return a custom 404 error."""
